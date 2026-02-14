986756e9c890146e6f5bcede4c9ddfb3d699ca2f
Ujjawal Rathore <rathoreujjawal868@gmail.com>
2026-02-14 20:38:00 +0530

    fix production animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    "animate-float",
+    "animate-burst",
+    "animate-fadeIn",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
